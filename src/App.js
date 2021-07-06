ff3ae95d3b838aeb14abce22b1eb47c97d723217
52917 <52917@etu.he2b.be>
2021-07-06 14:31:55 +0200

    comit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
+import Welcome from "./component/exo1.jsx";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     Hello warudo
+     <Welcome nom = 'Bono' age = '20'/>
     </div>
   );
 }
