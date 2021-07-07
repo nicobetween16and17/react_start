f79bf22f14e50f68dabfc6f1446e9bcc3e0f73d7
52917 <52917@etu.he2b.be>
2021-07-07 16:37:10 +0200

    compteur*

---
+++ src/component/compteur.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+const Compteur = function() {
+
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+    <h1>Compteur: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>
+        +
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        -
+      </button>
+    </>
+  );
+}
+export default Compteur;
